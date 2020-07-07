09f337bb7f9ce1b9a91816623af62bf9ede8c41a
Inari Aaltojärvi <ext.inari.aaltojarvi@cargotec.com>
2020-07-07 15:41:34 +0300

    Show more functionality implemented in Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,31 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+import './Blog.css'
+
+const Blog = ({ blog }) => {
+  const [showBlogMore, setShowBlogMore] = useState(false)
+
+  const showMore = () => {
+    setShowBlogMore(!showBlogMore)
+  }
+
+  return (
+    <div className="blog">
+      {blog.title}
+      <br></br>
+      {showBlogMore ? (
+        <>
+          {`Author: '${blog.author}'`}
+          <br></br>
+          {`Likes: ${blog.likes}`}
+          <br></br>
+          <button onClick={() => showMore()}>Hide</button>
+        </>
+      ) : (
+          <button onClick={() => showMore()}>Show more</button>
+        )
+      }
+    </div>
+  )
+}
 
 export default Blog
