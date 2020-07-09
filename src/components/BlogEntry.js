e371fa17ad3a89f0c438383f51db41c60f292cdb
Inari Aaltojärvi <ext.inari.aaltojarvi@cargotec.com>
2020-07-09 15:48:18 +0300

    Test for blog entry done.

--- src/components/BlogEntry.js
+++ src/components/BlogEntry.js
@@ -1,36 +1,48 @@
 import React, { useState } from 'react'
 
 const BlogEntry = ({ handlePost }) => {
-  const [inputs, setInputs] = useState({ title: '', author: '', url: '' })
+  // const [inputs, setInputs] = useState({ title: '', author: '', url: '' })
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   const addBlog = (event) => {
     event.preventDefault()
     const newObject = {
-      title: inputs.title,
-      author: inputs.author,
-      url: inputs.url
+      title: title,
+      author: author,
+      url: url
     }
     handlePost(newObject)
-    setInputs({ title: '', author: '', url: '' })
+    setTitle('')
+    setAuthor('')
+    setUrl('')
   }
 
-  const handleChange = (event) => {
-    event.persist()
-    setInputs(inputs => ({ ...inputs, [event.target.name]: event.target.value }))
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value)
   }
+  const handleAuthorChange = (event) => {
+    setAuthor(event.target.value)
+  }
+  const handleUrlChange = (event) => {
+    setUrl(event.target.value)
+  }
+
+
 
   return (
     <form onSubmit={(event) => addBlog(event)}>
       Create new blog entry
       <br></br>
       Title
-      <input id="title" value={inputs.title} name="title" type="text" onChange={handleChange} />
+      <input id="title" value={title} type="text" onChange={handleTitleChange} />
       <br></br>
       Author
-      <input id="author" value={inputs.author} name="author" type="text" onChange={handleChange} />
+      <input id="author" value={author} name="author" type="text" onChange={handleAuthorChange} />
       <br></br>
       Url
-      <input id="url" value={inputs.url} name="url" type="text" onChange={handleChange} />
+      <input id="url" value={url} name="url" type="text" onChange={handleUrlChange} />
       <br></br>
       <br></br>
       <button type="submit">Create</button>
--- src/components/BlogEntry.test.js
+++ src/components/BlogEntry.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, fireEvent, userEvent } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import BlogEntry from './BlogEntry'
 
@@ -11,24 +11,21 @@ test('<BlogEntry /> updates parent state and calls handleSubmit', () => {
   )
 
   const title = component.container.querySelector('#title')
-  const author = component.container.querySelector('#author')
-  const url = component.container.querySelector('#url')
-  const form = component.container.querySelector('form')
-
   fireEvent.change(title, {
     target: { value: 'testing of forms could be easier' }
   })
+  const author = component.container.querySelector('#author')
   fireEvent.change(author, {
     target: { value: 'I am the author' }
   })
+  const url = component.container.querySelector('#url')
   fireEvent.change(url, {
     target: { value: 'thisisaddress.com' }
   })
-
+  const form = component.container.querySelector('form')
   fireEvent.submit(form)
 
   expect(addBlog.mock.calls).toHaveLength(1)
-  console.log(addBlog.mock.calls[0][0])
   expect(addBlog.mock.calls[0][0].title).toBe('testing of forms could be easier')
   expect(addBlog.mock.calls[0][0].author).toBe('I am the author')
   expect(addBlog.mock.calls[0][0].url).toBe('thisisaddress.com')
