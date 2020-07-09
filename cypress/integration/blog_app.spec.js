030196ca1dc74d99d49bb6eca66451ef67b2cf03
Inari Aaltojärvi <ext.inari.aaltojarvi@cargotec.com>
2020-07-09 16:18:45 +0300

    Cypress installed for e2e testing

---
+++ cypress/integration/blog_app.spec.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+describe('Blog ', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:3000')
+    cy.contains('Please login')
+    cy.contains('Username')
+  })
+})
